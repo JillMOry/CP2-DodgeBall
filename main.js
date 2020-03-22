b8fdf10f0f310cb7bb7e86b5212731e229838b1c
JillMOry <oryjill@gmail.com>
2020-03-21 19:34:23 -0500

    CP2 dodgeball added comments

--- main.js
+++ main.js
@@ -50,9 +50,9 @@ const arrOfPeople = [
 	}
 ];
 
-const listOfPlayers = [];
-const blueTeam = [];
-const redTeam = [];
+const listOfPlayers = []; //arrayOfPeople become list of players
+const blueTeam = []; // listOfPlayers become a teammate
+const redTeam = []; // listOfPlayers become a teammate
 
 class Player {
 	constructor(
@@ -131,24 +131,33 @@ class redTeammate {
 	}
 }
 
-// lists all people that could be players
+// lists all people that could be players function that is called with the onclick
 const listPeopleChoices = () => {
+	//assigns listElement to the HTML element with the ID of people
 	const listElement = document.getElementById("people");
+	//mapp loops thru the arrOfPeople...
 	arrOfPeople.map((person) => {
 		if (person) {
+			// a list element is created mand
 			const li = document.createElement("li");
+			//a html button is created
 			const button = document.createElement("button");
+			// lables the button make player
 			button.innerHTML = "Make Player";
-
+			// listener event "moves" the eligible player from the list of people choices to the make player list.
 			button.addEventListener("click", function() {
 				makePlayer(person.id);
+				// removes element from list of people
 				listElement.removeChild(li);
 			});
+			// appends the li element to the const listElement
+			listElement.append(li);
+			// appends the "make player" button to the li element
 			li.appendChild(button);
+			// appends the person's name and skill set to the li element
 			li.appendChild(
 				document.createTextNode(person.name + " - " + person.skillSet)
 			);
-			listElement.append(li);
 		}
 	});
 };
@@ -158,9 +167,9 @@ const makePlayer = (id) => {
 	let person = arrOfPeople.find(function(player) {
 		return player.id == id;
 	});
-	// finds the index of the person id
+	// finds the index of the person id from arrOfPeople
 	let personIndex = arrOfPeople.indexOf(person);
-	// cuts the person /index
+	// cuts the person /index from arrOfPeople
 	arrOfPeople.splice(personIndex, 1);
 	// instantiates a new Player
 	let newPlayer = new Player(
@@ -176,20 +185,30 @@ const makePlayer = (id) => {
 	);
 	//sends to list of players array
 	listOfPlayers.push(newPlayer);
+	//creates a HTML element for new player
 	const listElement = document.getElementById("players");
+	// creates a list element
 	const li = document.createElement("li");
+	// appends the li with the player name
 	li.appendChild(document.createTextNode(newPlayer.name));
+	//creates a button in HTML doc
 	const blueButton = document.createElement("button");
+	// labels the button blue team
 	blueButton.innerHTML = "Blue Team";
+	//listener event removes the eligible player.
 	blueButton.addEventListener("click", function() {
 		makeBluePlayer(newPlayer.id);
+		//removes the list element from the Dodge Ball Player list
 		listElement.removeChild(li);
-	}); //create blue button
+	});
+	//appends the button to the list
 	li.appendChild(blueButton);
 	const redButton = document.createElement("button");
 	redButton.innerHTML = "Red Team";
+	//listener event removes the eligible player.
 	redButton.addEventListener("click", function() {
 		makeRedPlayer(newPlayer.id);
+		//removes the list element from the Dodge Ball Player list
 		listElement.removeChild(li);
 	});
 	li.appendChild(redButton);
@@ -199,7 +218,7 @@ const makePlayer = (id) => {
 };
 
 const makeBluePlayer = (id) => {
-	debugger;
+	// debugger;
 	let person = listOfPlayers.find(function(player) {
 		return player.id == id;
 	});
